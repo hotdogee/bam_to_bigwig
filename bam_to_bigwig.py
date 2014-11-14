bda79c14c325b096c4416e5d1f6a73bdda213670
Han Lin <hotdogee@gmail.com>
2014-11-14 11:36:18 -0500

    Check write permissions before calling rsem-bam2wig and wigToBigWig More informative error messages Hide output when running dependency checking

--- bam_to_bigwig.py
+++ bam_to_bigwig.py
@@ -49,6 +49,10 @@ Installation:
     * cp rsem-bam2wig /usr/local/bin
 
 Changelog:
+v1.3:
+* Check write permissions before calling rsem-bam2wig and wigToBigWig
+* More informative error messages
+* Hide output when running dependency checking 
 v1.2:
 * Added dependency checking
 * Better documentation
@@ -62,12 +66,49 @@ __version__ = '1.2'
 
 import os
 import sys
-import subprocess
 import tempfile
+import errno
+from subprocess import call, check_call, Popen, PIPE, STDOUT
 from optparse import OptionParser
 from contextlib import contextmanager
 import logging
 log = logging.getLogger(__name__)
+
+unix_signals_tsv = """1	SIGHUP	Exit	Hangup
+2	SIGINT	Exit	Interrupt
+3	SIGQUIT	Core	Quit
+4	SIGILL	Core	Illegal Instruction
+5	SIGTRAP	Core	Trace/Breakpoint Trap
+6	SIGABRT	Core	Abort
+7	SIGEMT	Core	Emulation Trap
+8	SIGFPE	Core	Arithmetic Exception
+9	SIGKILL	Exit	Killed
+10	SIGBUS	Core	Bus Error
+11	SIGSEGV	Core	Segmentation Fault
+12	SIGSYS	Core	Bad System Call
+13	SIGPIPE	Exit	Broken Pipe
+14	SIGALRM	Exit	Alarm Clock
+15	SIGTERM	Exit	Terminated
+16	SIGUSR1	Exit	User Signal 1
+17	SIGUSR2	Exit	User Signal 2
+18	SIGCHLD	Ignore	Child Status
+19	SIGPWR	Ignore	Power Fail/Restart
+20	SIGWINCH	Ignore	Window Size Change
+21	SIGURG	Ignore	Urgent Socket Condition
+22	SIGPOLL	Ignore	Socket I/O Possible
+23	SIGSTOP	Stop	Stopped (signal)
+24	SIGTSTP	Stop	Stopped (user)
+25	SIGCONT	Ignore	Continued
+26	SIGTTIN	Stop	Stopped (tty input)
+27	SIGTTOU	Stop	Stopped (tty output)
+28	SIGVTALRM	Exit	Virtual Timer Expired
+29	SIGPROF	Exit	Profiling Timer Expired
+30	SIGXCPU	Core	CPU time limit exceeded
+31	SIGXFSZ	Core	File size limit exceeded
+32	SIGWAITING	Ignore	All LWPs blocked
+33	SIGLWP	Ignore	Virtual Interprocessor Interrupt for Threads Library
+34	SIGAIO	Ignore	Asynchronous I/O"""
+unix_signals = dict([(-int(t[0]), t) for t in [line.split('\t') for line in unix_signals_tsv.split('\n')]])
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
@@ -105,6 +146,14 @@ def bam_to_wig(bam_filename, wig_filename, ignore_secondary=False, ignore_qc_fai
     file_prefix = os.path.splitext(bam_filename)[0]
     if not wig_filename:
         wig_filename = '%s.wig' % file_prefix
+    # check if we have permissions to write wig_filename if file exists or write its directory
+    if os.path.isfile(wig_filename):
+        if not os.access(wig_filename, os.W_OK):
+            log.critical('Write permission denied: %s' % (os.path.abspath(wig_filename)))
+            return False
+    elif not os.access(os.path.dirname(os.path.abspath(wig_filename)), os.W_OK):
+        log.critical('Write permission denied: %s' % (os.path.dirname(os.path.abspath(wig_filename))))
+        return False
     cl = ['rsem-bam2wig', bam_filename, wig_filename, file_prefix, '--no-fractional-weight']
     if ignore_secondary:
         cl.append('--ignore-secondary')
@@ -114,7 +163,18 @@ def bam_to_wig(bam_filename, wig_filename, ignore_secondary=False, ignore_qc_fai
         cl.append('--ignore-optical-pcr-duplicate')
     if ignore_supplementary:
         cl.append('--ignore-supplementary')
-    subprocess.check_call(cl, stderr=subprocess.STDOUT)
+    p = Popen(cl)
+    p.communicate()
+    rc = p.returncode
+    if rc != 0:
+        if rc < 0:
+            try:
+                log.critical('"%s" was terminated by signal %d: %s, %s, %s' % (' '.join(cl), -rc, unix_signals[rc][1], unix_signals[rc][2], unix_signals[rc][3]))
+            except KeyError:
+                log.critical('"%s" was terminated by signal %d' % (' '.join(cl), -rc))
+        else:
+            log.critical('"%s" terminated with non-zero return code: %d' % (' '.join(cl), rc))
+    return rc == 0, rc
 
 @contextmanager
 def indexed_bam(bam_filename):
@@ -132,16 +192,45 @@ def get_sizes_from_bam(bam_filename):
 
 def bam_to_sizes(bam_filename, chr_sizes_filename):
     chr_sizes = get_sizes_from_bam(bam_filename)
-    with open(chr_sizes_filename, "w") as f:
-        for chrom, size in chr_sizes:
-            f.write("%s\t%s\n" % (chrom, size))
+    try:
+        fp = open(chr_sizes_filename, "wb")
+    except IOError as e:
+        if e.errno == errno.EACCES:
+            log.critical('Write permission denied: %s' % (os.path.abspath(chr_sizes_filename)))
+            return False
+        # Not a permission error.
+        raise
+    else:
+        with fp:
+            for chrom, size in chr_sizes:
+                f.write("%s\t%s\n" % (chrom, size))
+    return True
 
 def wig_to_bigwig(wig_filename, bigwig_filename, chr_sizes_filename):
     file_prefix = os.path.splitext(wig_filename)[0]
     if not bigwig_filename:
         bigwig_filename = '%s.bigwig' % file_prefix
+    # check if we have permissions to write bigwig_filename if file exists or write its directory
+    if os.path.isfile(bigwig_filename):
+        if not os.access(bigwig_filename, os.W_OK):
+            log.critical('Write permission denied: %s' % (os.path.abspath(bigwig_filename)))
+            return False
+    elif not os.access(os.path.dirname(os.path.abspath(bigwig_filename)), os.W_OK):
+        log.critical('Write permission denied: %s' % (os.path.dirname(os.path.abspath(bigwig_filename))))
+        return False
     cl = ['wigToBigWig', wig_filename, chr_sizes_filename, bigwig_filename]
-    subprocess.check_call(cl, stderr=subprocess.STDOUT)
+    p = Popen(cl)
+    p.communicate()
+    rc = p.returncode
+    if rc != 0:
+        if rc < 0:
+            try:
+                log.critical('"%s" was terminated by signal %d: %s, %s, %s' % (' '.join(cl), -rc, unix_signals[rc][1], unix_signals[rc][2], unix_signals[rc][3]))
+            except KeyError:
+                log.critical('"%s" was terminated by signal %d' % (' '.join(cl), -rc))
+        else:
+            log.critical('"%s" terminated with non-zero return code: %d' % (' '.join(cl), rc))
+    return rc == 0, rc
 
 def main(bam_filename, bigwig_filename=None, use_tempfile=False, keep_tempfile=False, ignore_secondary=False, ignore_qc_fail=False, ignore_optical_pcr_duplicate=False, ignore_supplementary=False):
         #config = {'program': {'ucsc_bigwig' : 'wigToBigWig', 'rsem-bam2wig' : 'rsem-bam2wig'}}
@@ -160,22 +249,28 @@ def main(bam_filename, bigwig_filename=None, use_tempfile=False, keep_tempfile=F
     if use_tempfile:
         #Use a temp file to avoid any possiblity of not having write permission
         wig_filename = tempfile.NamedTemporaryFile(delete=False).name
-    bam_to_wig(bam_filename, wig_filename, ignore_secondary, ignore_qc_fail, ignore_optical_pcr_duplicate, ignore_supplementary)
-    sys.stdout.write('Generate wig file: %s ...Done\n' % wig_filename)
+    sys.stdout.write('Building wig file: %s ...\n' % wig_filename)
+    if not bam_to_wig(bam_filename, wig_filename, ignore_secondary, ignore_qc_fail, ignore_optical_pcr_duplicate, ignore_supplementary):
+        sys.exit(1)
+    sys.stdout.write('Done\n' % wig_filename)
     
     # generate sizes file
     chr_sizes_filename = '%s.sizes' % file_prefix
     if use_tempfile:
         #Use a temp file to avoid any possiblity of not having write permission
         chr_sizes_filename = tempfile.NamedTemporaryFile(delete=False).name
-    sys.stdout.write('Generate sizes file: %s' % chr_sizes_filename)
-    bam_to_sizes(bam_filename, chr_sizes_filename)
-    sys.stdout.write(' ...Done\n')
+    sys.stdout.write('Building sizes file: %s ...' % chr_sizes_filename)
+    if not bam_to_sizes(bam_filename, chr_sizes_filename):
+        sys.stdout.write(' Failed\n')
+        sys.exit(1)
+    sys.stdout.write(' Done\n')
     
     # run wigToBigWig
-    sys.stdout.write('Generate bigwig file: %s' % bigwig_filename)
-    wig_to_bigwig(wig_filename, bigwig_filename, chr_sizes_filename)
-    sys.stdout.write(' ...Done\n')
+    sys.stdout.write('Building bigwig file: %s ...' % bigwig_filename)
+    if not wig_to_bigwig(wig_filename, bigwig_filename, chr_sizes_filename):
+        sys.stdout.write(' Failed\n')
+        sys.exit(1)
+    sys.stdout.write(' Done\n')
     
     # remove temp files
     if not keep_tempfile:
@@ -188,6 +283,11 @@ def dependences_exist():
     wigToBigWig from UCSC (http://hgdownload.cse.ucsc.edu/admin/exe/)
     rsem-bam2wig from RSEM (http://deweylab.biostat.wisc.edu/rsem/)
     """
+    try:
+        from subprocess import DEVNULL # py3k
+    except ImportError:
+        import os
+        DEVNULL = open(os.devnull, 'wb')
     all_good = True
     try:
         import pysam
@@ -195,12 +295,12 @@ def dependences_exist():
         all_good = False
         log.critical('Missing dependency: pysam (https://github.com/pysam-developers/pysam)')
     try:
-        subprocess.call(['wigToBigWig'])
+        call(['wigToBigWig'], stdout=DEVNULL, stderr=STDOUT)
     except OSError:
         all_good = False
         log.critical('Missing dependency: wigToBigWig from UCSC (http://hgdownload.cse.ucsc.edu/admin/exe/)')
     try:
-        subprocess.call(['rsem-bam2wig'])
+        call(['rsem-bam2wig'], stdout=DEVNULL, stderr=STDOUT)
     except OSError:
         all_good = False
         log.critical('Missing dependency: rsem-bam2wig from RSEM (http://deweylab.biostat.wisc.edu/rsem/)')
@@ -234,5 +334,9 @@ if __name__ == '__main__':
         ignore_qc_fail=options.ignore_qc_fail,
         ignore_optical_pcr_duplicate=options.ignore_optical_pcr_duplicate,
         ignore_supplementary=options.ignore_supplementary)
-    for bam_filename in args:
-        main(bam_filename, **kwargs)
+    try:
+        for bam_filename in args:
+            main(bam_filename, **kwargs)
+    except KeyboardInterrupt:
+        sys.stdout.write('\n')
+        log.info('Aborted by keyboard interrupt')
